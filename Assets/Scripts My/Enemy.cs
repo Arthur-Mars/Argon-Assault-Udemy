510feeed2d6123acdd2f1ef5dca1f302638cd5d4
Arthur <the.arthur.of.war@gmail.com>
2019-12-01 20:56:30 +0200

    Lesson 100 - Argon Assault
    Detecting Enemy Collision
    
    Ensuring enemies explode when they are hit with the bullet particles

---
+++ Assets/Scripts My/Enemy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [Header("Particles")]
+    public ParticleSystem trail;
+    public ParticleSystem explosion;
+
+    [Header("Sound FX")]
+    public AudioClip explosionSound;
+
+    AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnParticleCollision(GameObject other) // detects collision with the particles (bullets)
+    {
+        explosion.Play();
+        audioSource.PlayOneShot(explosionSound);
+        Destroy(this.gameObject);
+    }
+}
