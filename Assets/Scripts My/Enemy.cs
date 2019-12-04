c83d81ea72aaf6c1873bf16309cac7bf9f90d5ad
Arthur <the.arthur.of.war@gmail.com>
2019-12-05 00:37:28 +0200

    Lesson 106 - Argon Assault
    Introducing Encapsulation

--- Assets/Scripts My/Enemy.cs
+++ Assets/Scripts My/Enemy.cs
@@ -12,10 +12,14 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
         //AddNonTriggerBoxCollider(); commented function out due to box colliders being way too large
     }
 
@@ -28,8 +32,11 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other) // detects collision with the particles (bullets)
     {
+        scoreBoard.ScoreHit(scorePerHit);
         GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity); // create deathFX, at transform position, and don't rotate it
         fx.transform.parent = parent;
+        print("particle location = " + fx.transform.position);
+        print("enemy location = " + this.gameObject.transform.position);
         Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts My/ScoreBoard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // this is how we get and access the UI elements such as text and whatnot 
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scoreIncrease = 12;
+
+    int score = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void ScoreHit(int scoreIncrease) 
+    {
+        score = score + scoreIncrease;
+        scoreText.text = score.ToString();
+    }
+}
